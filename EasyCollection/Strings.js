00455bf5ddf4eb2b4c173482ade9773a376ad5a8
Ivan Abdulov <iabdulov@roox.ru>
2022-01-22 13:35:44 +0300

    Strings section. Tasks: strStr.

--- EasyCollection/Strings.js
+++ EasyCollection/Strings.js
@@ -340,3 +340,99 @@ const myAtoi3 = (s) => {
 //         console.log('failed');
 //     }
 // })
+
+
+// Implement strStr().
+// Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+// Clarification:
+// What should we return when needle is an empty string? This is a great question to ask during an interview.
+// For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    return haystack.indexOf(needle);
+};
+
+// tests
+// const arr = [
+// {
+//     haystack: "hello",
+//     needle: "ll",
+//     output: 2
+// },
+// {
+//     haystack: "aaaaa",
+//     needle: "bba",
+//     output:-1,
+// },
+// {
+//     haystack: "",
+//     needle: "",
+//     output: 0
+// },
+// ];
+// arr.forEach(({haystack, needle, output}) => {
+//     const res = strStr(haystack, needle);
+//     if (res === output) {
+//         console.log('res', res, 'expected', output, 'passed');
+//     } else {
+//         console.log('res', res);
+//         console.log('exp', output);
+//         console.log('failed');
+//     }
+// })
+
+
+// Count and Say
+// The count-and-say sequence is a sequence of digit strings defined by the recursive formula:
+// countAndSay(1) = "1"
+// countAndSay(n) is the way you would "say" the digit string from countAndSay(n-1), which is then converted into a different digit string.
+// To determine how you "say" a digit string, split it into the minimal number of groups so that each group is a contiguous section all of the same character. Then for each group, say the number of characters, then say the character. To convert the saying into a digit string, replace the counts with a number and concatenate every saying.
+
+// For example, the saying and conversion for digit string "3322251":
+// Output 23321511
+// Given a positive integer n, return the nth term of the count-and-say sequence.
+
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var countAndSay = function(n) {
+    console.log(n);
+    if(n === 1){
+        return '1';
+    }
+    return countAndSay(n-1);
+};
+
+// tests
+const arr = [
+    {
+        n: 1,
+        output: "1"
+    },
+    {
+        n: 4,
+        output: "1211"
+        // Explanation:
+        // countAndSay(1) = "1"
+        // countAndSay(2) = say "1" = one 1 = "11"
+        // countAndSay(3) = say "11" = two 1's = "21"
+        // countAndSay(4) = say "21" = one 2 + one 1 = "12" + "11" = "1211"
+    }
+]
+arr.forEach(({n, output}) => {
+    const res = countAndSay(n);
+    if (res === output) {
+        console.log('res', res, 'expected', output, 'passed');
+    } else {
+        console.log('res', res);
+        console.log('exp', output);
+        console.log('failed');
+    }
+})
+
